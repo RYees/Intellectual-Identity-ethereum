851e0a6b6666ab8fddf5bb4bcb73821b14122fa0
netes <toygame8888@gmail.com>
2022-08-23 18:22:52 +0300

    nft changes

--- smart_contract/contracts/Lock.sol
+++ smart_contract/contracts/Lock.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.9;
 
 // Import this file to use console.log
-import "hardhat/console.sol";
+import "../node_modules/hardhat/console.sol";
 
 contract Lock {
     uint public unlockTime;
