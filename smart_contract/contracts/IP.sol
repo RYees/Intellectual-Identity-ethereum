5839bd311386feb4ca92d265dc0141cdd42adff2
netes <toygame8888@gmail.com>
2022-08-30 15:35:36 +0300

    changes made with contract

--- smart_contract/contracts/IP.sol
+++ smart_contract/contracts/IP.sol
@@ -4,17 +4,18 @@ import "hardhat/console.sol";
 
 contract IP {
     address public owner;
+  
     struct Count{
         uint count;
     }
-
+    
     mapping (address =>Count) newcount;
     
-     enum Status {
+    enum Status {
         Pending,
         Accepted,
         Rejected
-    }
+     }
 
     struct IParameter {
         string IPname;
@@ -22,7 +23,8 @@ contract IP {
         string country;
         string addressplace;    
         string symbol;   
-        string timestamp;
+        uint256 timestamp;
+        bool isRegistered;
         Status[] status; 
     }
 
@@ -41,35 +43,50 @@ contract IP {
         
     mapping (uint => uint) bal;
 
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+ 
     function setIP(address _address, string memory _IPname, string memory _fullname, string memory _country, string memory _addressplace, string memory _symbol,Status a) public {
         property[_address].IPname = _IPname;
         property[_address].fullname = _fullname;
         property[_address].country = _country;
         property[_address].addressplace = _addressplace;
         property[_address].symbol = _symbol;
+        property[_address].timestamp = block.timestamp;
         property[_address].status.push(a);
-        //status = Status.Pending;
         newcount[_address].count = 0;
+        property[_address].isRegistered = true;
         intelProperty.push(_address);
     }
-    
-    function chageStatus(address player, Status val) public returns(bool sucess) {
-        require(msg.sender == owner);
-        property[player].status.push(val);
-        newcount[player].count++; 
+   
+    function changeStatus(address _address, Status val) public onlyOwner returns(bool) {
+        property[_address].status.push(val);
+        newcount[_address].count++; 
         return true;
     }
 
-    function getStatus(address player) public view returns(Status status) {
-        return property[player].status[newcount[player].count];
+    function getStatus(address _address) public view returns(Status status) {
+        return property[_address].status[newcount[_address].count];
     }
 
     function getAllIP() view public returns (address[] memory) {
         return intelProperty;
     }
 
-    function getIP(address _address) public view returns (string memory, string memory, string memory, string memory, string memory) {
-        return (property[_address].IPname, property[_address].fullname, property[_address].country, property[_address].addressplace, property[_address].symbol);
+    function getIP(address _address) public view returns (string memory, string memory, string memory, string memory, string memory, uint256, Status status) {
+        uint num = newcount[_address].count;
+        IParameter memory u = property[_address];
+        return (
+            u.IPname, 
+            u.fullname,
+            u.country,
+            u.addressplace, 
+            u.symbol,
+            u.timestamp, 
+            u.status[num]
+            );
     }
 
     function countIP() view public returns (uint) {
