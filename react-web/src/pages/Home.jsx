2da85840e8c196b6173021c1fa2b4b3f88520f55
netes <toygame8888@gmail.com>
2022-09-06 13:29:05 +0300

    images

--- react-web/src/pages/Home.jsx
+++ react-web/src/pages/Home.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from 'react';
+import banner from "../assets/pexel.jpg";
+import eth from "../assets/shubham.jpg";
+import '../css/App.css';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <div className=''>
+      <div>
+        <img src={banner} alt="home-image" className='banner'/>
+        <div className='overlay'></div>
+      </div>
+      
+      <div className='homecontain'>
+        <div className='first'>
+          <h1>Secure your <italic>
+            <strong>Intellectul Property</strong>    
+          </italic> in the blockchain!</h1>
+          <p>Prevent others from using yours' invention and decide who is allowed to produce, sell or import your invention.</p>
+        </div>
+
+        <div className='two'>
+           <img src={eth} alt="ethereum image" className='imagetwo'/>
+        </div>
+      </div>    
+    </div>
+  </>
   )
 }
 
