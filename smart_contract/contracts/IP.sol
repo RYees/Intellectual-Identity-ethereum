e0a18a851e30fb8e0f62419955571aea9d1b59eb
netes <toygame8888@gmail.com>
2022-09-18 21:01:18 +0300

    import bidder

--- smart_contract/contracts/IP.sol
+++ smart_contract/contracts/IP.sol
@@ -2,8 +2,11 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.1;
 import "hardhat/console.sol";
+import "./IP_Nfts.sol";
 
 contract IP {
+    IpItem nft = new IpItem();
+
     address public owner;
 
     struct Count{
@@ -154,64 +157,3 @@ contract IP {
 
 }
 
-contract IPbidder {
-    bidder[] bids;
-    uint public bidCount;
-    struct IPowner {
-        //address IPowneradddress;
-        string ownerIPname;
-        uint bidValue;
-        address bidderAddress;
-    }
-    
-    constructor(){
-        bidCount = 0;
-    }
-    struct bidder{
-        mapping(address => IPowner[]) ipowner;
-    }
-    //mapping(address => IPowner[]) ipowner;
-    
-    function setIPbidder(address _address, string memory _ownerIPname, uint _bidvalue, address _bidderaddress) public {         
-        bidder storage r = bids.push();
-        r.ipowner[_address].push(IPowner(_ownerIPname, _bidvalue, _bidderaddress));      
-        bidCount++;
-    }
-    
-    //mapping(address => IPowner[]) ipowner;
-    //address[] public ys;
-    mapping(address => mapping(string => mapping(uint => IPowner))) public ips;
-
-   
-     function setIPbidder(address _address, uint _id, string memory _ownerIPname, uint _bidvalue, address _bidderaddress) public {         
-    //     // bidder storage r = bids.push();
-    //   ipowner[_address].push(IPowner(_ownerIPname, _bidvalue, _bidderaddress));      
-         ips[_address][_ownerIPname][_id] = IPowner(_ownerIPname, _bidvalue, _bidderaddress);
-    //     employeeAccts.push(_address);
-    }
-    
-
-    function getbidderinfo(address _address, uint i) public view returns(IPowner[] memory){
-        return (bids[i].ipowner[_address]);
-    }
-
-    function countJars() view public returns (uint) {
-        return bids.length;
-    }
-
-    function bidLoop(address _address) public view returns (IPowner[] memory) {
-         IPowner[] memory ipss = new IPowner[](bidCount);
-        for (uint i = 0; i < bidCount; i++) {             
-             bidder storage parameter = bids[i];
-            // IPowner storage lBid = parameter.ipowner[_address];
-             ipss = parameter.ipowner[_address];
-             
-             //jarnam[i] = parameter.jarname;  
-            //   Cookie storage lBid = parameter.cookies[_address];
-            //   cook[_address] = lBid;  
-            // ips = parameter.ipowner[_address];        
-        }
-        return(ipss);
-     }
-
-}
