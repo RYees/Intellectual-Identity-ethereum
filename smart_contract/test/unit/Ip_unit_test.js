09db7e4bb7ddcbd039fe377aebc25852e5af6911
netes <toygame8888@gmail.com>
2022-10-01 16:51:18 +0300

    contract changes

--- smart_contract/test/unit/Ip_unit_test.js
+++ smart_contract/test/unit/Ip_unit_test.js
@@ -1,4 +1,4 @@
-const { expect } = require('chai');
+const { assert, expect } = require('chai');
 
 var chai = require('chai');
 
@@ -9,24 +9,33 @@ describe('IP Unit Test', function () {
       await IP.deployed();
     });
 
-    beforeEach(async function () {
-      await IP.setIP('0x5B38Da6a701c568545dCfcB03FcB875f56beddC4', 'skkns','mary joe', 'Indis', 'mos12', 'SK');
-    })
+    // beforeEach(async function () {
+    //   await IP.setIP('0x5B38Da6a701c568545dCfcB03FcB875f56beddC4', 'skkns','mary joe', 'India', 'street12', 'https://skku.com');
+    //   await IP.setIP('0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2', 'skkns','mary joe', 'India', 'street12', 'https://skku.com');
+    // })
+    
+    it('storing a value', async function () {
+      //  await Employer.setEmployee('0x5B38Da6a701c568545dCfcB03FcB875f56beddC4', 'emp1','34.09', '98.43', '20', '3','4');
+      await IP.setIP('0x5B38Da6a701c568545dCfcB03FcB875f56beddC4', 'skkns','mary joe', 'India', 'street12', 'https://skku.com');
+      //expect((await Employer.getEmployee('0x5B38Da6a701c568545dCfcB03FcB875f56beddC4')).toString()).to.equal('34.09', '98.43', '3:30');
+    });
 
     // it('registering an Ip details', async function () {
-    //   //  await Employer.setEmployee('0x5B38Da6a701c568545dCfcB03FcB875f56beddC4', 'emp1','34.09', '98.43', '20', '3','4');
-    //   expect((await IP.getMember()).toString()).to.equal('skkns','mary joe', 'Indis', 'mos12', 'SK', 0);
+    //   await IP.setIP('0x5B38Da6a701c568545dCfcB03FcB875f56beddC4', 'skkns','mary joe', 'India', 'street12', 'https://skku.com');
+    //   expect((await IP.getPendingIP()).toString()).to.equal('');
+    //   assert.equal(IP.countPendingIPs().toString(), 0, 'pending count is');
+    //  // expect((await IP.countPendingIPs()).toNumber()).to.equal(1);
     //   //expect((await Employer.getEmployee('0x5B38Da6a701c568545dCfcB03FcB875f56beddC4')).toString()).to.equal('34.09', '98.43', '3:30');
     // });
     
-    beforeEach(async function () {
-        await IP.changeStatus(0,1);
-    })
+    // beforeEach(async function () {
+    //     await IP.changeStatus(0,1);
+    // })
 
-    it('retrieve returns a Ip status', async function () {
-    //  await Employer.setEmployee('0x5B38Da6a701c568545dCfcB03FcB875f56beddC4', '34.09', '98.43', '3:30');
-     expect((await IP.getStatus(1)).toString()).to.equal(1);
-    }); 
+    // it('retrieve returns a Ip status', async function () {
+    // //  await Employer.setEmployee('0x5B38Da6a701c568545dCfcB03FcB875f56beddC4', '34.09', '98.43', '3:30');
+    //  expect((await IP.getStatus(1)).toString()).to.equal(1);
+    // }); 
 
     // it('retrieve returns a count of previously stored', async function () {
     //   //  await Employer.setEmployee('0x5B38Da6a701c568545dCfcB03FcB875f56beddC4', '34.09', '98.43', '3:30');
@@ -42,4 +51,35 @@ describe('IP Unit Test', function () {
     //  // expect(await Employer.getBalance()).to.be.within(0);
     //   expect((await Employer. getBalance()).toNumber()).to.equal(0); 
     // });
+
+    // it('Verifies the blogger ', async function () {
+
+    //   await IP.setIP('0x5B38Da6a701c568545dCfcB03FcB875f56beddC4', 'skkns','mary joe', 'India', 'street12', 'https://skku.com');
+     
+      // const bloggerCount = await IP.countPendingIPs()
+
+      // const expectedNumber = 0
+
+      // assert.equal(bloggerCount.toString(), expectedNumber)
+
+   // })
+
+    // it('Verifies the blogger number count to be 1', async function () {
+
+    //       const bloggerCount = await IP.countPendingIPs()
+    
+    //       const expectedNumber = '1'
+    
+    //       assert.equal(bloggerCount.toString(), expectedNumber)   
+    
+    //     })
+
+    it('retrieve returns a count of previously stored', async function () {
+      //  await Employer.setEmployee('0x5B38Da6a701c568545dCfcB03FcB875f56beddC4', '34.09', '98.43', '3:30');
+        expect((await IP.countPendingIPs()).toNumber()).to.equal(0);
+    }); 
+        
+
   });
+  
+ 
