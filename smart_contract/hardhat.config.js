77340edd7a4dc87303d35ae9759b180a2365bdcc
netes <toygame8888@gmail.com>
2022-10-02 17:39:51 +0300

    removing files

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -11,9 +11,10 @@ module.exports = {
       hardhat: {},
       goerli: {
          url: API_URL,
-         accounts: [`0x${PRIVATE_KEY}`]
-      }
+         accounts: [`0x${PRIVATE_KEY}`],
+         allowUnlimitedContractSize: true
+      },
    },
-}
+};
 
 
--- smart_contract/test/unit/Ip_unit_test.js
+++ smart_contract/test/unit/Ip_unit_test.js
@@ -5,8 +5,8 @@ var chai = require('chai');
 describe('IP Unit Test', function () {
     before(async function () {
       IP = await ethers.getContractFactory('IP');
-      IP = await IP.deploy();
-      await IP.deployed();
+      IPs = await IP.deploy();
+      await IPs.deployed();
     });
 
     // beforeEach(async function () {
@@ -15,12 +15,14 @@ describe('IP Unit Test', function () {
     // })
     
   it('storing a value', async function () {
-     await IP.setIP('0x5B38Da6a701c568545dCfcB03FcB875f56beddC4', 'skkns','mary joe', 'India', 'street12', 'https://skku.com');
-     //await IP.setIP('0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2', 'skkns','mary joe', 'India', 'street12', 'https://skku.com');
+    // await IP.setIP('0x5B38Da6a701c568545dCfcB03FcB875f56beddC4', 'skkns','mary joe', 'India', 'street12', 'https://skku.com');
+     await IPs.setIP('0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2', 'skkns','mary joe', 'India', 'street12', 'https://skku.com');
   });
   
   // it('get array value', async function () {
-  //   expect((await IP.getPendingIP()).toString()).to.equal('0');
+  //   //expect((await IP.getPendingIP()).toNumber()).to.equal(0);
+  //   await IPs.setIP('0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2', 'skkns','mary joe', 'India', 'street12', 'https://skku.com');
+  //   expect((await IPs.getPendingIP()).toNumber()).to.equal(0);
   // });
 
   // it('get a IP value', async function () {
@@ -40,15 +42,17 @@ describe('IP Unit Test', function () {
   // }); 
   
   it('retrieve returns a pending count of previously stored', async function () {
-      expect((await IP.countPendingIPs()).toNumber()).to.equal(0);
+    await IPs.setIP('0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2', 'skkns','mary joe', 'India', 'street12', 'https://skku.com');  
+    expect((await IPs.countPendingIPs()).toNumber()).to.equal(0);
   }); 
 
   it('retrieve returns a accepted count of previously stored', async function () {
-      expect((await IP.countAcceptedIPs()).toNumber()).to.equal(0);
+    await IPs.changeStatus(0,1);  
+    expect((await IPs.countAcceptedIPs()).toNumber()).to.equal(0);
   }); 
 
   it('retrieve returns a rejected count of previously stored', async function () {
-    expect((await IP.countRejectedIPs()).toNumber()).to.equal(0);
+    expect((await IPs.countRejectedIPs()).toNumber()).to.equal(0);
   }); 
    
     // it('Verifies the blogger ', async function () {
@@ -73,7 +77,11 @@ describe('IP Unit Test', function () {
     
     //     })
 
- 
+    
+    it('retrieve returns a pending count of previously stored', async function () {
+      await IPs.setIPbidder1('0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2', 'skkns', 1000, '0x5B38Da6a701c568545dCfcB03FcB875f56beddC4') 
+      expect((await IPs.getbidderinfo('0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2')).toString()).to.equal('0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2');
+    }); 
     
   });
   
