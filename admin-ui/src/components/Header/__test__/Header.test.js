4d932d1d03e021ceae26d5d34a3fd3e76fd64247
Rehmet Yeshanew <ryfgmz87@gmail.com>
2022-10-15 17:35:25 -0400

    react testing

--- admin-ui/src/components/Header/__test__/Header.test.js
+++ admin-ui/src/components/Header/__test__/Header.test.js
@@ -1,5 +1,15 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Header from '../Header';
+import { createMemoryHistory } from 'history';
+import { BrowserRouter } from 'react-router-dom';
+
+const TestHeader = () => {
+   return (
+   <BrowserRouter>
+      <Header />
+   </BrowserRouter>
+   )
+};
 
 describe("Header", ()=>{
 
@@ -13,18 +23,22 @@ describe("Header", ()=>{
     });
 
     it('should render same text passed into title prop', () => {
-    render(<Header/>);
+    render(<TestHeader/>);
     const headingElement = screen.getByTestId("header-start");
     expect(headingElement).toBeInTheDocument();
     });
 
-    // //getByRole only looks for one matching element, if it found more it will throw an error
-    // it('renders app heading by the h2 tag found in the Header component with name of the heading that is `TestName`', () => {
-    //     render(<Header title="My Header"/>);
-    //     const headingElement = screen.getByRole("heading", {name: 'FakeName'});
-    //     expect(headingElement).toBeInTheDocument();
-    // });
-
+    it('should pass', () => {
+        const history = createMemoryHistory({ initialEntries: ['/'] });
+        // const { getByText } = render(
+        //   <Router history={history}>
+        //     <ButtonLogin />
+        //   </Router>
+        // );
+        // expect(history.location.pathname).toBe('/home');
+        fireEvent.click(getByText('Ips'));
+        expect(history.location.pathname).toBe('/');
+      });
     // it('renders app heading by the header tag found in the Header component with name of the heading that is `My Header` ', () => {
     //     render(<Header title="My Header"/>);
     //     // we made the change below is because there are two headings in the Header component and since getByRole only work for returning one element we specify a single heading declaration so that it cna only found one match where the title is now 'My Header'
@@ -33,50 +47,5 @@ describe("Header", ()=>{
     // });
 
 
-    // it('renders app heading by the header tag found in the Header component with the title name `header`', async () => {
-    //     render(<Header title="My Header"/>);
-    //     const headingElement = screen.getByTitle("Header");
-    //     expect(headingElement).toBeInTheDocument();
-    // });
-
-
-    // it('renders app heading by the test id that is assigned on the DOM elements of Header Components', async () => {
-    //     render(<Header title="My Header"/>);
-    //     const headingElement = screen.getByTestId("header-1");
-    //     expect(headingElement).toBeInTheDocument();
-    // });
-
-    // //Find By 
-    // it('renders app heading by the find by attribute', async () => {
-    //     render(<Header title="My Header"/>);
-    //     const headingElement = await screen.findByText(/my header/i);
-    //     expect(headingElement).toBeInTheDocument();
-    // });
-
-    // //QueryBy
-    // it('renders app heading by the query by attribute', async () => {
-    //     render(<Header title="My Header"/>);
-    //     const headingElement = screen.queryByText(/FakeName/i);
-    //     expect(headingElement).toBeInTheDocument();
-    // });
-
-    // it('renders app heading by the query by attribute', async () => {
-    //     render(<Header title="My Header"/>);
-    //     const headingElement = screen.queryByText(/my header/i);
-    //     expect(headingElement).toBeInTheDocument();
-    // });
-
-    // it('renders app heading by the get by attribute, by shows the element is not found in the dom document', async () => {
-    //     render(<Header title="My Header"/>);
-    //     const headingElement = screen.queryByText(/dogs/i); //there is no dogs in the webpage but since i used queryBy, the testing will not throw an error, instead it just return a null value HOWEVER if i was using getBy it will throw an error before it goes to the next line
-    //     expect(headingElement).not.toBeInTheDocument();
-    // });
-
-    // //getAllByRole
-    // it('renders app heading by the getall by attribute', async () => {
-    //     render(<Header title="My Header"/>);
-    //     const headingElements = screen.getAllByRole("heading"); //getAllByRole returns and array
-    //     expect(headingElements.length).toBe(3);
-    // });
-
+   
 });
