01a5b3411d45f154e52a70d4de004cf020f9beb7
Rehmet Yeshanew <ryfgmz87@gmail.com>
2023-03-13 18:06:45 -0400

    pushed changes

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,13 +1,15 @@
 //https://eth-goerli.g.alchemy.com/v2/GcrVjbu5A8Me4tmHm8rJStfcvJvOG_en
 
 require('@nomiclabs/hardhat-waffle');
+require('dotenv').config({path: '/.env'});
+const { SECRET_KEY } = process.env;
 
 module.exports = {
   solidity: '0.8.1',
   networks: {
     goerli: {
       url: 'https://eth-goerli.g.alchemy.com/v2/GcrVjbu5A8Me4tmHm8rJStfcvJvOG_en',
-      accounts: ['53dc4cad3c6eadea0a956d66fd28fdec6fd66e8117f76b97c15a0e9bfad59494'],
+      accounts: [`0x${SECRET_KEY}`],
       allowUnlimitedContractSize: true
     },
   },
